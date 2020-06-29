f379363cec7a43595cdb3406578a89c4649554e5
fumiyaso <yasojima@seeds-std.co.jp>
2020-06-30 00:14:02 +0900

    aboutページにogp画像適応

--- src/pages/about.js
+++ src/pages/about.js
@@ -14,6 +14,9 @@ export default ({ data, location }) => (
       pagetitle="ESSENTIALについて"
       pagedesc="食べ物についての情報を発信しているサイトです。"
       pagepath={location.pathname}
+      pageimg={data.about.childImageSharp.original.src}
+      pageimgw={data.about.childImageSharp.original.width}
+      pageimgh={data.about.childImageSharp.original.height}
     />
     <div className="eyecatch">
       <figure>
@@ -62,6 +65,11 @@ export const query = graphql `
       childImageSharp {
         fluid(maxWidth: 1600) {
           ...GatsbyImageSharpFluid_withWebp
+        }
+        original {
+          height
+          src
+          width
         }
       }
     }
