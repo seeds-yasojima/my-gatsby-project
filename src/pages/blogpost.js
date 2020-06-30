492eff19bcfe3dc8a39024667cc46829f3d0b3b3
fumiyaso <yasojima@seeds-std.co.jp>
2020-07-01 01:11:34 +0900

    ブログ記事 graphql当て込み

--- src/pages/blogpost.js
+++ src/pages/blogpost.js
@@ -1,29 +1,37 @@
 import React from 'react'
+import { graphql } from 'gatsby'
+import Img from 'gatsby-image'
 import Layout from '../components/layout'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faClock, faFolderOpen } from "@fortawesome/free-regular-svg-icons"
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
-export default () => (
+export default ({ data }) => (
     <Layout>
         <div className="eyecatch">
             <figure>
-            <img src="images-baseblog/eyecatch.jpg" alt="アイキャッチ画像の説明" />
+            <Img
+                fluid={data.contentfulBlogPost.eyecatch.fluid}
+                alt={data.contentfulBlogPost.eyecatch.description}
+            />
             </figure>
         </div>
         <article className="content">
             <div className="container">
-            <h1 className="bar">記事のタイトル</h1>
+            <h1 className="bar">{data.contentfulBlogPost.title}</h1>
             <aside className="info">
-                <time dateTime="XXXX-XX-XX">
+                <time dateTime={data.contentfulBlogPost.publishDate}>
                     <FontAwesomeIcon icon={faClock} />
-                    XXXX年XX月XX日
+                    {data.contentfulBlogPost.publishDateJP}
                 </time>
                 <div className="cat">
                 <FontAwesomeIcon icon={faFolderOpen} />
                 <ul>
-                    <li className="スラッグ">カテゴリーＡ</li>
-                    <li className="スラッグ">カテゴリーＢ</li>
+                    {data.contentfulBlogPost.category.map(cat => (
+                        <li className={cat.categorySlug} key={cat.id}>
+                            {cat.category}
+                        </li>
+                    ))}
                 </ul>
                 </div>
             </aside>
@@ -52,3 +60,24 @@ export default () => (
         </article>
     </Layout>
 )
+
+export const query = graphql`
+    query {
+        contentfulBlogPost {
+            title
+            publishDateJP: publishDate(formatString: "YYYY年MM月DD日")
+            publishDate
+            category {
+                category
+                categorySlug
+                id
+            }
+            eyecatch {
+                fluid(maxWidth: 1600) {
+                    ...GatsbyContentfulFluid_withWebp
+                }
+                description
+            }
+        }
+    }
+`
