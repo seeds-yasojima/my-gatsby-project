46054da60f613ad558cc4f215ed11144afd70a33
fumiyaso <yasojima@seeds-std.co.jp>
2020-07-06 00:57:54 +0900

    メタ情報プロパティを記述

--- src/templates/blogpost-template.js
+++ src/templates/blogpost-template.js
@@ -12,6 +12,8 @@ import {
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 import { BLOCKS } from "@contentful/rich-text-types"
 import useContentfulImage from "../utils/useContentfulImage"
+import { documentToPlainTextString } from "@contentful/rich-text-plain-text-renderer"
+import SEO from "../components/seo"
 
 const options = {
   renderNode: {
@@ -39,8 +41,18 @@ const options = {
   },
 }
 
-export default ({ data, pageContext }) => (
+export default ({ data, pageContext, location }) => (
   <Layout>
+    <SEO
+      pagetitle={data.contentfulBlogPost.title}
+      pagedesc={`${documentToPlainTextString(
+        data.contentfulBlogPost.content.json
+      ).slice(0, 70)}…`}
+      pagepath={location.pathname}
+      blogimg={`https:${data.contentfulBlogPost.eyecatch.file.url}`}
+      pageimgw={data.contentfulBlogPost.eyecatch.file.details.image.width}
+      pageimgh={data.contentfulBlogPost.eyecatch.file.details.image.height}
+    />
     <div className="eyecatch">
       <figure>
         <Img
@@ -113,6 +125,15 @@ export const query = graphql`
           ...GatsbyContentfulFluid_withWebp
         }
         description
+        file {
+          details {
+            image {
+              height
+              width
+            }
+          }
+          url
+        }
       }
       content {
         json
