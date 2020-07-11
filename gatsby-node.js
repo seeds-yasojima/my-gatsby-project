c7973423ab120b4ea0e34b880dc8e26db2dc9022
fumiyaso <yasojima@seeds-std.co.jp>
2020-07-11 17:23:26 +0900

    カテゴリーページの生成実装

--- gatsby-node.js
+++ gatsby-node.js
@@ -21,6 +21,18 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
           }
         }
       }
+      allContentfulCategory {
+        edges {
+          node {
+            categorySlug
+            id
+            category
+            blogpost {
+              title
+            }
+          }
+        }
+      }
     }
   `)
 
@@ -29,6 +41,7 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
     return
   }
 
+  // ブログ詳細ページ出力
   blogresult.data.allContentfulBlogPost.edges.forEach(({ node, next, previous }) => {
     createPage({
       path: `/blog/post/${node.slug}/`,
@@ -41,6 +54,7 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
     })
   })
 
+  // ブログ一覧ページ出力
   const blogPostsPerPage = 6 // 1ページに表示する記事の数
   const blogPosts = blogresult.data.allContentfulBlogPost.edges.length // 記事の総数
   const blogPages = Math.ceil(blogPosts / blogPostsPerPage) // 記事一覧ページの総数
@@ -51,8 +65,38 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
       component: path.resolve("src/templates/blog-template.js"),
       context: {
         skip: blogPostsPerPage * i,
-        limit: blogPostsPerPage
+        limit: blogPostsPerPage,
+        currentPage: i + 1, // 現在のページ番号
+        isFirst: i + 1 === 1, // 最初のページ
+        isLast: i + 1 === blogPages // 最後のページ
       }
+    })
+  })
+
+  // カテゴリページ出力
+  blogresult.data.allContentfulCategory.edges.forEach(({ node }) => {
+    const catPostsPerPage = 6 // 1ページに表示する記事の数
+    const catPosts = node.blogpost.length // 記事の総数
+    const catPages = Math.ceil(catPosts / catPostsPerPage) // 記事一覧ページの総数
+
+    Array.from({ length: catPages }).forEach((_, i) => {
+      createPage({
+        path:
+          i === 0
+            ? `/cat/${node.categorySlug}/`
+            : `/cat/${node.categorySlug}/${i + 1}/`,
+        component: path.resolve(`./src/templates/cat-template.js`),
+        context: {
+          catid: node.id,
+          catname: node.category,
+          catslug: node.categorySlug,
+          skip: catPostsPerPage * i,
+          limit: catPostsPerPage,
+          currentPage: i + 1, // 現在のページ番号
+          isFirst: i + 1 === 1, // 最初のページ
+          isLast: i + 1 === catPages // 最後のページ
+        }
+      })
     })
   })
 }
