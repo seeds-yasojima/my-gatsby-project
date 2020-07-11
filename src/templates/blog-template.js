af1f9171c004d0e6257ed6fe7c0b8e9a55fc7f4b
fumiyaso <yasojima@seeds-std.co.jp>
2020-07-11 17:24:31 +0900

    ブログ一覧にページャーを追加

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -4,7 +4,13 @@ import Img from 'gatsby-image'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-export default ({ data, location }) => (
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faChevronLeft,
+  faChevronRight
+} from "@fortawesome/free-solid-svg-icons"
+
+export default ({ data, location, pageContext }) => (
   <Layout>
     <SEO
       pagetitle="ブログ"
@@ -30,6 +36,36 @@ export default ({ data, location }) => (
             </article>
           ))}
         </div>
+        <ul className="pagenation">
+          {!pageContext.isFirst && (
+            <li className="prev">
+              <Link
+                to={
+                  pageContext.currentPage === 2
+                     ? `/blog/`
+                     : `/blog/${pageContext.currentPage - 1}`
+                }
+                rel="prev"
+              >
+                <FontAwesomeIcon icon={faChevronLeft} />
+                <span>前のページ</span>
+              </Link>
+            </li>
+          )}
+          {!pageContext.isLast && (
+            <li className="next">
+              <Link
+                to={
+                  `/blog/${pageContext.currentPage + 1}/`
+                }
+                rel="next"
+              >
+                <span>次のページ</span>
+                <FontAwesomeIcon icon={faChevronRight} />
+              </Link>
+            </li>
+          )}
+        </ul>
       </div>
     </section>
   </Layout>
